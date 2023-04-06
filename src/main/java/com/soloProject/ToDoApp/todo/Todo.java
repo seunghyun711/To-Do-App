477cf57b157787005a87bac095e15ee347f44700
seunghyun711 <seunghyun711@gmail.com>
2023-04-06 11:32:03 +0900

    Todo 엔티티 클래스 생성

--- src/main/java/com/soloProject/ToDoApp/todo/Todo.java
+++ src/main/java/com/soloProject/ToDoApp/todo/Todo.java
@@ -1,2 +1,36 @@
-package com.soloProject.ToDoApp.todo;public class Todo {
+package com.soloProject.ToDoApp.todo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title; // 해야할 일
+
+    @Column(nullable = false)
+    private int todoOrder; // 순서
+
+    @Column(nullable = false)
+    private boolean completed; // 진행 여부
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setTodoOrder(int todoOrder) {
+        this.todoOrder = todoOrder;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
