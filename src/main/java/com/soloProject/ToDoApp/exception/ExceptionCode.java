af97e1369d15cf6b3c3f2074c47a64beb52c6878
seunghyun711 <seunghyun711@gmail.com>
2023-04-06 14:24:45 +0900

    exception 관련 로직 생성

---
+++ src/main/java/com/soloProject/ToDoApp/exception/ExceptionCode.java
@@ -0,0 +1,20 @@
+package com.soloProject.ToDoApp.exception;
+
+import lombok.Getter;
+
+// 에러 관련 메시지
+public enum ExceptionCode {
+    TITLE_EXISTS(404,"THIS TITLE IS ALREADY EXISTED"), // 이미 존재하는 할일인 경우
+    ORDERS_EXISTS(404,"THIS TODO_ORDER IS ALREADY EXISTED"); // 우선순위가 겹치는 경우
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
