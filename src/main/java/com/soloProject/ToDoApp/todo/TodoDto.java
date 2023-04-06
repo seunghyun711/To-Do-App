960d4efc755d94f35d9b90efdf32dc7de7f74621
seunghyun711 <seunghyun711@gmail.com>
2023-04-06 13:13:15 +0900

    build.gradle validation 의존성 추가 & TodoDto - Post 생성

---
+++ src/main/java/com/soloProject/ToDoApp/todo/TodoDto.java
@@ -0,0 +1,44 @@
+package com.soloProject.ToDoApp.todo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Getter
+public class TodoDto {
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class Post{
+        @Positive
+        private long id;
+
+        @Valid
+        @NotNull(message = "할 일을 입력하세요.")
+        private String title;
+
+        @Valid
+        @NotNull(message = "우선 순위를 입력하세요.")
+        private int todoOrder;
+
+        @Valid
+        @NotNull(message = "진행 여부를 입력하세요(true/false")
+        private boolean completed;
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setTodoOrder(int todoOrder) {
+            this.todoOrder = todoOrder;
+        }
+
+        public void setCompleted(boolean completed) {
+            this.completed = completed;
+        }
+    }
+}
