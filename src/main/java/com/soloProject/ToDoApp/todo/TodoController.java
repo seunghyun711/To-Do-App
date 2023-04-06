49fc6d923f3ba6e073c5beaddf6078af59dcc843
seunghyun711 <seunghyun711@gmail.com>
2023-04-06 13:04:12 +0900

    TodoController 생성

---
+++ src/main/java/com/soloProject/ToDoApp/todo/TodoController.java
@@ -0,0 +1,14 @@
+package com.soloProject.ToDoApp.todo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/todo")
+public class TodoController {
+
+}
