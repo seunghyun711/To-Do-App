3d6449aca14ac4bd4960025c44cfb27f984428a2
seunghyun711 <seunghyun711@gmail.com>
2023-04-06 15:38:48 +0900

    ExceptionCode 추가

--- src/main/java/com/soloProject/ToDoApp/exception/ExceptionCode.java
+++ src/main/java/com/soloProject/ToDoApp/exception/ExceptionCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 // 에러 관련 메시지
 public enum ExceptionCode {
     TITLE_EXISTS(404,"THIS TITLE IS ALREADY EXISTED"), // 이미 존재하는 할일인 경우
-    ORDERS_EXISTS(404,"THIS TODO_ORDER IS ALREADY EXISTED"); // 우선순위가 겹치는 경우
+    ORDERS_EXISTS(404,"THIS TODO_ORDER IS ALREADY EXISTED"), // 우선순위가 겹치는 경우
+    ID_NOT_FOUND(404, "THIS ID DOES NOT EXIST"); // 존재하지 않는 id인 경우
 
     @Getter
     private int status;
