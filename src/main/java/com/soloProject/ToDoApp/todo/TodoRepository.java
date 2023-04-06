038e5fc617ea94a9629d954e84b5afec5c127fd3
seunghyun711 <seunghyun711@gmail.com>
2023-04-06 13:04:57 +0900

    TodoRepository 생성

---
+++ src/main/java/com/soloProject/ToDoApp/todo/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.soloProject.ToDoApp.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
